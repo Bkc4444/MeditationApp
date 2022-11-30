e8bfe503973dfaf1c0dca8450c793cde2f341ec0
Brandon Carroll <bkc4444@gmail.com>
2022-11-29 23:39:26 -0800

    Made all the files needed to start making the full app. Customized the background of the apps as well.

--- app/src/main/java/com/example/meditationapp/MainActivity.kt
+++ app/src/main/java/com/example/meditationapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.meditationapp
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TO DO
+        // make on clicks for each button to link to the other fragemnts to pull them up
     }
 }
---
+++ app/src/main/java/com/example/meditationapp/Meditation2Fragment.kt
@@ -0,0 +1,28 @@
+package com.example.meditationapp
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Meditation2Fragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_meditation2)
+
+        val constraintLayout: ConstraintLayout = findViewById(R.id.meditationFragment2)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(3000)
+        animationDrawable.setExitFadeDuration(3000)
+        animationDrawable.start()
+    }
+
+    //TO DO
+    // Make home button when clicked return to ActiviyMain
+
+    //When Play/Pause button is pushed it plays the meditation or pauses it
+}
