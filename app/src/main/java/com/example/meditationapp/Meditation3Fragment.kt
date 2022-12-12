ea9dc663263d49e2f0675e494d0428d51b10c485
Brandon Carroll <bkc4444@gmail.com>
2022-12-11 19:56:46 -0800

    Messed with the timing on the gradient for fading in and out

--- app/src/main/java/com/example/meditationapp/Meditation3Fragment.kt
+++ app/src/main/java/com/example/meditationapp/Meditation3Fragment.kt
@@ -1,6 +1,7 @@
 package com.example.meditationapp
 
 import android.graphics.drawable.AnimationDrawable
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,30 +12,61 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class Meditation3Fragment : Fragment() {
+    private lateinit var  mediaPlayer: MediaPlayer
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Return to MainFragment when button clicked return
         val view = inflater.inflate(R.layout.fragment_meditation3, container, false)
         val homeBtn : Button = view.findViewById(R.id.homeBtn3)
-        homeBtn.setOnClickListener{
-            val mainFrag =  MainFragment()
-            val transaction = fragmentManager?.beginTransaction()
-            transaction?.replace(R.id.nav_graph, mainFrag)?.commit()
-        }
-        //Controls the background
-        val constraintLayout: ConstraintLayout = view.findViewById(R.id.meditationFragment3)
-        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
-        animationDrawable.setEnterFadeDuration(3000)
-        animationDrawable.setExitFadeDuration(3000)
-        animationDrawable.start()
+        val playAndPauseBtn : Button = view.findViewById(R.id.startStopBtn3)
+
+        goHome(homeBtn)
+        playAndPause(playAndPauseBtn)
+        backgroundControls(view)
         return view
     }
 
-    //TO DO
-    // Make home button when clicked return to ActiviyMain
+    private fun backgroundControls(view: View) {
+        //Controls the background
+        val constraintLayout: ConstraintLayout = view.findViewById(R.id.meditationFragment3)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(4500)
+        animationDrawable.setExitFadeDuration(4500)
+        animationDrawable.start()
+    }
+
+    // Return to MainFragment when button clicked return
+    private fun Meditation3Fragment.goHome(homeBtn: Button) {
+        homeBtn.setOnClickListener {
+            val mainFrag = MainFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.nav_graph, mainFrag)?.commit()
+        }
+    }
 
     //When Play/Pause button is pushed it plays the meditation or pauses it
+    private fun playAndPause(playAndPauseBtn: Button) {
+        playAndPauseBtn.setOnClickListener {
+            if (!this::mediaPlayer.isInitialized) {
+                mediaPlayer = MediaPlayer.create(requireContext(), R.raw.forsleep)
+            }
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                return@setOnClickListener
+            }
+            mediaPlayer.start()
+        }
+    }
+
+    // Stop the sound file from playing in the same spot if you go to a new fragment
+    override fun onDestroy(){
+        if (this::mediaPlayer.isInitialized){
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+        super.onDestroy()
+    }
 }
